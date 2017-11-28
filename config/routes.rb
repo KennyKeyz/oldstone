14a0a7d7ced8d56a79763f129a0164f6ca279537
vagrant <vagrant@precise32.(none)>
2017-11-28 11:52:44 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+
+  get 'contact', to: 'static_pages#contact'
+
+  get 'about-us', to: 'static_pages#about'
+
+
+
+  get 'help', to: 'static_pages#help'
+
+
+  root to: 'static_pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
